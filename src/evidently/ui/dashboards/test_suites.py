d728260a36d82dbdf510ad4ae1c3a6c7858f5497
Joel Mills <joelarimills@gmail.com>
2024-08-21 14:10:01 -0400

    text as y axis update 4

--- src/evidently/ui/dashboards/test_suites.py
+++ src/evidently/ui/dashboards/test_suites.py
@@ -163,7 +163,7 @@ class DashboardPanelTestSuite(DashboardPanel):
                 go.Bar(
                     name="",
                     x=dates,
-                    y=[test.name for _ in range(len(dates))],
+                    y=[test.name for _ in dates],
                     marker_color=[get_color(test, d) for d in dates],
                     hovertemplate=_get_test_hover(test.name, hover_params[test]),
                     customdata=[get_description(test, d) for i, d in enumerate(dates)],
@@ -196,7 +196,6 @@ class DashboardPanelTestSuite(DashboardPanel):
                 automargin=True,
             ),
         )
-        fig.update_yaxes(showticklabels=True)
         return fig
 
 
